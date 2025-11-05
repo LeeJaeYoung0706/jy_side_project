1aa041f85a3bfac10eea29136a0154bcc28f8f0d
jaesoon3737 <wodud4019@gmail.com>
2025-11-05 20:25:02 +0900

    exception , service , repository

--- master/src/main/java/com/mes_jyproject/master/study/exceptions/ResourceNotFoundException.java
+++ master/src/main/java/com/mes_jyproject/master/study/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.mes_jyproject.master.study.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.NOT_FOUND) // advice 에서 잡지 못하는 경우에도 Spring 에서 해당 코드로 HTTP 를 보냄
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
