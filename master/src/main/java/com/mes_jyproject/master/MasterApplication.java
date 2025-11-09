6b5115cd5d8d150996faca072aa9cedaab90f24c
leejae <148510209+LeeJaeYoung0706@users.noreply.github.com>
2025-11-09 10:36:57 +0900

    swagger 기본처리 콘피그 만드는 방법말고 기본적 application main문에서 처리하는 방법

--- master/src/main/java/com/mes_jyproject/master/MasterApplication.java
+++ master/src/main/java/com/mes_jyproject/master/MasterApplication.java
@@ -1,11 +1,36 @@
 package com.mes_jyproject.master;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "testUserAuditing")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Master Application",
+				description = "MES Application with MSA Study example",
+				version = "V2",
+				contact = @Contact(
+						name = "Lee JAe Young",
+						email = "wodud4019@gmail.com",
+						url = "MES"
+				),
+				license = @License(
+						name = "apache 2.0",
+						url = "http://mes.study.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "",
+				url = "http://mes.study.com"
+		)
+)
 public class MasterApplication {
 
 	public static void main(String[] args) {
